52ff086686c7df1671182a58ca3f74dcf504cf19
shishka351 <mn@loranc.ru>
2020-12-05 18:36:11 +0300

    some beauty

--- tasksHTML/script.js
+++ tasksHTML/script.js
@@ -1,15 +1,10 @@
 var number = 0;
 
 window.onload = function() {
-    // Get the input field
     var input = document.getElementById("text");
-
-    // Execute a function when the user releases a key on the keyboard
     input.addEventListener("keyup", function(event) {
         if (event.keyCode === 13) {
-            // Cancel the default action, if needed
             event.preventDefault();
-            // Trigger the button element with a click
             document.getElementById("btn").click();
         }
     });
