f412d9231e761803792d85a95e7aa896b0fb2bac
Savva Antonyuk <savvaanto@SavvaOS.local>
2020-12-12 16:10:03 +0300

    taskscards

--- tasksHTML/script.js
+++ tasksHTML/script.js
@@ -10,25 +10,14 @@ window.onload = function() {
     });
 }
 
-function Done(boxId) {
-    var box = document.getElementById(boxId);
-    if (box.src == 'https://d29fhpw069ctt2.cloudfront.net/clipart/89484/preview/checkbox_unchecked_preview_48d2.png')
-    {
-        box.src = "https://static.tildacdn.com/tild3463-3138-4137-a232-333538373261/web-2018010114204555.png";
-    }
-    else
-    {
-        box.src = "https://d29fhpw069ctt2.cloudfront.net/clipart/89484/preview/checkbox_unchecked_preview_48d2.png"
-    }
-}
-
 function NewTask() {
     number = number + 1
     var id = "'" + "box" + number + "'";
     var text = document.getElementById("text").value;
     var tasks = document.getElementById("tasks");
     var task = document.createElement("div");
-    task.innerHTML = '<img id=' + id + ' class="checkbox" src="https://d29fhpw069ctt2.cloudfront.net/clipart/89484/preview/checkbox_unchecked_preview_48d2.png" onclick="Done(' + id + ')"/>' + text
+    task.innerHTML = '<input type="checkbox" id=' + id + ' class="checkbox"/>' + text;
+    task.setAttribute("class", "task");
     tasks.appendChild(task);
     document.getElementById("text").value = ""
 }
